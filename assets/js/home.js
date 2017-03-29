032bf7bf6c98df47dd05a100fb412694fb34210d
Wendy Reyes <wendy.reyessan@gmail.com>
2017-03-29 13:28:18 -0500

    subiendo bienvenida

---
+++ assets/js/home.js
@@ -0,0 +1,25 @@
+
+var nombre = document.getElementById('nombre-coder');
+var apellido = document.getElementById('apellido-coder');
+var correo = document.getElementById('correo-coder');
+var pass = document.getElementById('pass-coder');
+
+var objetoCoder = JSON.parse(localStorage.getItem("nuevaCoder"));
+
+nombre.innerText=objetoCoder.nombre;
+apellido.innerText=objetoCoder.apellido;
+correo.innerText=objetoCoder.correo;
+pass.innerText=objetoCoder.pass;
+
+function Coder(nombre, apellido, correo, pass){
+
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.correo = correo;
+	this.pass = pass;
+
+};
+
+document.getElementsByTagName("button")[0].addEventListener("click",function(){
+		//window.location=".html"
+	})
