c0d1314062f8ba0513d0271293cb8de27e2d8ca2
Wendy Reyes <wendy.reyessan@gmail.com>
2017-03-29 12:54:16 -0500

    subiendo funcionalidad registrar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 
+document.getElementsByTagName("button")[0].addEventListener("click",function(){
+
+	window.location="register.html";
+
+});
+
+document.getElementsByTagName("button")[1].addEventListener("click",function(){
+
+	window.location="login.html";
+
+});
